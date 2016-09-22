599a0ad4e0b9c960799c4036c8c5e0010d4f62a8
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-21 21:29:07 -0700

    Added functions/objects

--- CS172_HW03_02/CS172_HW03_02/Source.cpp
+++ CS172_HW03_02/CS172_HW03_02/Source.cpp
@@ -6,6 +6,7 @@
 ////////////////////
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class QuadraticEquation
@@ -17,5 +18,43 @@ public:
 	int c;
 	//
 
+	// constructor
+	QuadraticEquation()
+	{
+		a = 0;
+		b = 0;
+		c = 0;
+	}
+
+	// get function for a
+	// get function for b
+	// get function for c
+
+	// function that returns the discriminant
+	double getDiscriminant (int a, int b, int c)
+	{
+		return ((b * b) - (4 * a * c));
+	}
+
+	// function that returns the first root
+	double getRoot1(int a, int b, int c)
+	{
+		return (((-b) + sqrt((b * b) - (4 * a * c))) / (2 * a));
+	}
+
+	// function that returns the second root
+	double getRoot2(int a, int b, int c)
+	{
+		if (getDiscriminant() >= 0)
+			return (((-b) - sqrt((b * b) - (4 * a * c))) / (2 * a));
+		else
+			return (0);
+	}
 
 };
+
+int main ()
+{
+
+	return 0;
+}
