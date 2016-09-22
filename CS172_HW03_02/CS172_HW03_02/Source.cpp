aac3e898c5b372647f56e98278a7609268eb9c6d
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-21 21:13:39 -0700

    Initial commit Exercise 2 HW 3

---
+++ CS172_HW03_02/CS172_HW03_02/Source.cpp
@@ -0,0 +1,21 @@
+////////////////////
+// Ian Fisher
+// CS 172
+// HW03_02
+// 9/21/16
+////////////////////
+
+#include <iostream>
+using namespace std;
+
+class QuadraticEquation
+{
+public:
+	// data field
+	int a;
+	int b;
+	int c;
+	//
+
+
+};
