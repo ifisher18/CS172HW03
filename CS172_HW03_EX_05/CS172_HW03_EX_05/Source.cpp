d0852f213c2052b51b0bd02338b6e76ac985530a
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-26 20:01:29 -0700

    Created Program/Set up

---
+++ CS172_HW03_EX_05/CS172_HW03_EX_05/Source.cpp
@@ -0,0 +1,17 @@
+///////////////////
+// Ian Fisher
+// CS172_HW03_EX_05
+// 9/26/16
+//////////////////
+
+#include <iostream>
+
+using namespace std;
+
+class MyInteger
+{
+public:
+	// data field
+	int value;
+
+};
