04a6643552da4fa5d7739fac90c5ab57dd083cef
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-26 19:58:09 -0700

    Created Program/Set up framework

---
+++ CS172_HW03_EX_04/CS172_HW03_EX_04/Source.cpp
@@ -0,0 +1,19 @@
+////////////////////
+// Ian Fisher
+// CS172_HW03_EX_04
+// 9/26/16
+///////////////////
+
+#include <iostream>
+
+using namespace std;
+
+// sort characters in a string
+
+int main ()
+{
+	cout << "Enter a string s: ";
+	cin >> string;
+
+	cout << "The sorted string is: " << // etc.
+}
