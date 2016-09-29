b3280db9eba17919e1e8afe88a74eba462a735b5
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-28 21:32:38 -0700

    Final Comments-- DONE!

--- CS172_HW03_EX_03/CS172_HW03_EX_03/Source.cpp
+++ CS172_HW03_EX_03/CS172_HW03_EX_03/Source.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 class EvenNumber // define class EvenNumber
 {
-public:
+public: // this data field is public (nothing is private)
     // data field
     int value;
     //
 
-    // constructor
+    // constructor that sets default value of 'value' to 0
     EvenNumber()
     {
         value = 0;
@@ -26,13 +26,13 @@ public:
         value = number;
     }
 
-    // function
+    // function that gets 'value'
     int GetValue()
     {
         return value;
     }
 
-    // function
+    // function that gets the next even number based on if value entered is even or odd
     EvenNumber getNext()
     {
         // if statement that checks if value is even or odd
@@ -48,18 +48,19 @@ public:
         }
     }
 
-    // function
+    // function that gets the previous even number if value entered is even or odd
     EvenNumber getPrevious()
     {
+		// if statement that checks if modulus is 0 or not (even or odd value)
         if (value % 2 == 0) {
-            EvenNumber wildcat = EvenNumber();
-            wildcat.setValue(value - 2);
-            return wildcat;
+            EvenNumber wildcat = EvenNumber(); // creates new EvenNumber object
+            wildcat.setValue(value - 2); // setting number to previous even number if value entered is even
+            return wildcat; // returns the value after the alteration
         }
         else {
-            EvenNumber bluebird = EvenNumber();
-            bluebird.setValue(value - 1);
-            return bluebird;
+            EvenNumber bluebird = EvenNumber(); // creates new EvenNumber object
+            bluebird.setValue(value - 1); // of the value entered was odd, the previous even number would be one less than odd number
+            return bluebird; // returns the new set value
         }
     }
 };
@@ -67,11 +68,9 @@ public:
 int main ()
 {
     EvenNumber var1 = EvenNumber(); // new EvenNumber type
-    var1.setValue(16);
-
+    var1.setValue(16); // this sets the value that will then be used in above functions to determine the next and prev. even numbers
     cout << "The starting even value is " << var1.GetValue() << "." << endl;
     cout << "The next even number is: " << var1.getNext().GetValue() << "." << endl; // performing getNext on even number object
     cout << "The previous even number is: " << var1.getPrevious().GetValue() << "." << endl; // perfomring getPrevious on even number object
-
     return 0;
 }
