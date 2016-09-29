3ae3b66d10a3b3bde1c66b7aed17715f556e56a8
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-28 21:18:37 -0700

    Added final comments-- DONE! (This is EX03_01)

--- CS172__Hw03/CS172__Hw03/Source.cpp
+++ CS172__Hw03/CS172__Hw03/Source.cpp
@@ -11,26 +11,27 @@ using namespace std;
 class Fan // creates a class called "Fan"
 {
 public:
-	int speed; // data field
+	// data field
+	int speed;
 	bool On;
 	double radius;
 
 	// function that gets a value speed (accessor function)
 	int getspeed()
 	{
-		return speed;
+		return speed; // returns the value for speed
 	}
 
 	// function that gets a value radius (accessor function)
 	double getradius()
 	{
-		return radius;
+		return radius; // returns the value for radius
 	}
 
 	// function that gets a value On (accessor function)
 	bool getOn()
 	{
-		return On;
+		return On; // returns the value for On
 	}
 
 	// function that sets speed to input value
@@ -50,7 +51,7 @@ public:
 	{
 		if (OnorOff == false) // if statement that checks if fan is on or off
 		{
-			On = false;
+			On = false; // if fan is off, sets boolean to false
 		}
 		else
 		{
@@ -71,12 +72,12 @@ int main()
 {
 	Fan F1, F2; // declares two fan objects
 
-	// For first fan, assigns values to variables
+	// For first fan, assigns values to variables (speed, radius, on/off)
 	F1.speed = 3;
 	F1.radius = 10;
 	F1.On = true;
 
-	// for second fan, assigns values to variables
+	// for second fan, assigns values to variables (speed, radius, on/off)
 	F2.speed = 2;
 	F2.radius = 5;
 	F2.On = false;
@@ -93,9 +94,9 @@ int main()
 
 	// calls get function for Fan 1 and couts to terminal
 	cout << "The fan properties for the first fan are: " << endl;
-	cout << "-- The fan speed is: " << F1.getspeed() << endl;
-	cout << "-- The fan radius is: " << F1.getradius() << endl;
-	if (F1.getOn() == 1)
+	cout << "-- The fan speed is: " << F1.getspeed() << endl; // calls get speed function that sets feed to needed value
+	cout << "-- The fan radius is: " << F1.getradius() << endl; // calls getradius function as well
+	if (F1.getOn() == 1) // if statement that checks if fan is on or off
 		cout << "-- The fan is on" << endl;
 	else
 		cout << "-- The fan is off" << endl;
@@ -105,7 +106,7 @@ int main()
 	cout << "The fan properties for the second fan are: " << endl;
 	cout << "-- The fan speed is: " << F2.getspeed() << endl;
 	cout << "-- The fan radius is: " << F2.getradius() << endl;
-	if (F2.getOn() == 1)
+	if (F2.getOn() == 1) // same thing as above-- checks if fan is on or off
 		cout << "-- The fan is on" << endl;
 	else
 		cout << "-- The fan is off" << endl;
