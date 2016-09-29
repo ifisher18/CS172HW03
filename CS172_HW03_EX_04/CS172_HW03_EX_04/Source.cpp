6a15585822881d3f960ecc451e259dff30f86205
Ian Fisher <ifisher18@my.whitworth.edu>
2016-09-28 20:12:08 -0700

    Major changes. getting there

--- CS172_HW03_EX_04/CS172_HW03_EX_04/Source.cpp
+++ CS172_HW03_EX_04/CS172_HW03_EX_04/Source.cpp
@@ -5,15 +5,40 @@
 ///////////////////
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 // sort characters in a string
 
+string sort(string& s)
+{
+	bool organized = true;
+
+	
+	s.size();
+	do {
+		organized = true; // flag
+		for (int i = 0; i < s.size() - 1; i++)
+		{
+			if (s[i] > s[i + 1])
+			{
+				char temp = s[i];
+				s[i] = s[i + 1];
+				s[i + 1] = temp;
+				organized = false;
+			}
+		}
+	} while (!organized);
+	
+	return s;
+}
+
 int main ()
 {
+	string input;
 	cout << "Enter a string s: ";
-	cin >> string;
-
-	cout << "The sorted string is: " << // etc.
+	cin >> input;
+	for (int i = 0;
+	cout << "The sorted string is: " << sort(input) << endl;
 }
